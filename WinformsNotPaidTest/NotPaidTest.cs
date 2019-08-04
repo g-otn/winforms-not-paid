d0fb64268064a342468dbd9ecf8eacb47f16f505
g-otn <g-otn@hotmail.com>
2019-08-04 01:54:25 -0300

    Create unit test project

---
+++ WinformsNotPaidTest/NotPaidTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Forms;
+using WinformsNotPaid;
+
+namespace WinformsNotPaidTest
+{
+    [TestClass]
+    public class NotPaidTest
+    {
+        private Form createTestForm()
+        {
+            Form form = new Form();
+            form.Width = 200;
+            form.Height = 100;
+            form.Padding = new Padding(50);
+
+            Label label = new Label();
+            label.Text = "Please pay or I'll dissapear";
+            label.AutoSize = true;
+
+            form.Controls.Add(label);
+
+            return form;
+        }
+
+        [TestMethod]
+        public void TestPastDueDate()
+        {
+            DateTime dueDate = new DateTime(2018, 1, 1);
+            int daysDeadLine = 30;
+
+            Form form = createTestForm();
+
+            Assert.IsTrue(form.Controls.Count == 1); // Has label
+
+            form.ChangeNotPaidOpacity(dueDate, daysDeadLine);
+
+            Assert.IsTrue(form.Opacity == 0);        // Past due date
+            Assert.IsTrue(form.Controls.Count == 0); // Label removed
+        }
+    }
+}
---
+++ WinformsNotPaidTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WinformsNotPaidTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WinformsNotPaidTest")]
+[assembly: AssemblyCopyright("Copyright © g-otn (https://github.com/g-otn) 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("51fbdba9-215e-4c39-8088-1e902d25d55b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
