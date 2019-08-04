0a27ba65547ba1bba8c95b9b7995de5904342243
g-otn <g-otn@hotmail.com>
2019-08-04 01:53:08 -0300

    Create project

---
+++ WinformsNotPaid/NotPaid.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformsNotPaid
+{
+    public static class NotPaid
+    {
+        public static void ChangeNotPaidOpacity(this Form form, DateTime dueDate, int daysDeadline)
+        {
+            if (DateTime.Compare(DateTime.Today, dueDate) > 0)             // Past due date
+            {
+                form.Opacity = 0;
+
+                // Removes all controls so it's not possible to use them even while invisible
+                foreach (Control control in form.Controls)
+                {
+                    form.Controls.Remove(control);
+                }
+            }
+            else if (dueDate.Subtract(DateTime.Today).Days < daysDeadline) // Inside deadline
+            {
+                form.Opacity = 1 - ((double)daysDeadline / dueDate.Subtract(DateTime.Today).Days);
+            }
+        }
+    }
+}
