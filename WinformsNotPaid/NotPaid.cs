31e393f3dd05313984694dbf4d7d431a324f91fd
g-otn <g-otn@hotmail.com>
2019-08-04 03:37:29 -0300

    Add XML documentation to ChangeNotPaidOpacity

--- WinformsNotPaid/NotPaid.cs
+++ WinformsNotPaid/NotPaid.cs
@@ -1,14 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WinformsNotPaid
 {
     public static class NotPaid
     {
+        /// <summary>
+        /// Changes the <see cref="Form.Opacity"/> depending of how close <see cref="DateTime.Today"/> is from <paramref name="dueDate"/>.
+        /// The <see cref="Form.Opacity"/> does not change if the number of days until <paramref name="dueDate"/> 
+        /// is higher or equal to <paramref name="daysDeadline"/>.
+        /// </summary>
+        /// <param name="dueDate">The date when the <see cref="Form"/> should be invisible and the Form.Controls should be cleared.</param>
+        /// <param name="daysDeadline">The number of days the deadline has and how many days before the <paramref name="dueDate"/> 
+        /// the <see cref="Form"/> will start dissapearing.</param>
         public static void ChangeNotPaidOpacity(this Form form, DateTime dueDate, int daysDeadline)
         {
             if (DateTime.Compare(DateTime.Today, dueDate) > 0)             // Past due date
